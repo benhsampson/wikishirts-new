4dd474671e66d074dd62679388d54bb7414e01e4
Benjamin Sampson <bensampsondev@gmail.com>
2018-10-23 21:42:32 +1100

    More deployment testing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const url = require('url');
-const queryString = require('querystring');
-const wtf = require('wtf_wikipedia');
-const fetch = require('node-fetch');
+const path = require('path');
+// const bodyParser = require('body-parser');
+// const url = require('url');
+// const queryString = require('querystring');
+// const wtf = require('wtf_wikipedia');
+// const fetch = require('node-fetch');
 // const markdownRouter = require('express-markdown-router');
 // const cloudinary = require('cloudinary');
 // const aws = require('aws-sdk');
@@ -13,8 +14,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 // app.use(markdownRouter(__dirname + '/pages'));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,103 +24,103 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Something was sent back' });
 });
 
-app.get('/api', async (req, res) => {
-  const search = req.query.search;
-
-  const response = await fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${search}&limit=3`);
-  const body = await response.json();
-
-  if (response.status !== 200) {
-    throw Error(body.message);
-  }
-
-  const descriptionPromises = [];
-
-  body[1].forEach((title, index) => {
-    descriptionPromises.push(new Promise(async (resolve, reject) => {
-      try {
-        resolve(await wtf.fetch(title).then((doc) => doc.sentences(0).text()));
-      } catch (exception) {
-        reject(exception);
-      }
-    }));
-  });
-
-  Promise.all(descriptionPromises).then((descriptions) => {
-    const shirts = body[1].map((title, index) => ({
-      name: `${title}`,
-      description: descriptions[index],
-    }));
-
-    res.send(shirts);
-  });
-});
-
-const CHARACTER_LIMIT = 500;
-
-app.get('/api/single', async (req, res) => {
-  const title = req.query.title;
-
-  const response = await wtf.fetch(title)
-    .then((doc) => {
-      const sentences = doc.sentences().map(({ data: { text } }) => text);
-
-      let reachedLimit = false;
-      let text = '';
-
-      for (let i = 0; i < sentences.length; i++) {
-        const proposedText = text + ' ' + sentences[i];
-
-        if (proposedText.length <= CHARACTER_LIMIT && !reachedLimit) {
-          text = proposedText.replace(/  +/g, ' ').trim();
-        } else {
-          reachedLimit = true;
-        }
-      }
-
-      res.send({
-        name: doc.options.title,
-        content: doc.data.sections[0].html(),
-        text,
-        links: doc.data.sections[0].links().map(({ page }) => page),
-        pageId: doc.options.pageID,
-      });
-    })
-    .catch((exception) => {
-      console.log('Critical error, could\'t view the freaking shirt.');
-    });
-});
-
-// aws.config.update({
-//   region: 'ap-southeast-2',
-//   accessKeyId: process.env.AWSAccessKeyId,
-//   secretAccessKey: process.env.AWSSecretKey,
+// app.get('/api', async (req, res) => {
+//   const search = req.query.search;
+//
+//   const response = await fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${search}&limit=3`);
+//   const body = await response.json();
+//
+//   if (response.status !== 200) {
+//     throw Error(body.message);
+//   }
+//
+//   const descriptionPromises = [];
+//
+//   body[1].forEach((title, index) => {
+//     descriptionPromises.push(new Promise(async (resolve, reject) => {
+//       try {
+//         resolve(await wtf.fetch(title).then((doc) => doc.sentences(0).text()));
+//       } catch (exception) {
+//         reject(exception);
+//       }
+//     }));
+//   });
+//
+//   Promise.all(descriptionPromises).then((descriptions) => {
+//     const shirts = body[1].map((title, index) => ({
+//       name: `${title}`,
+//       description: descriptions[index],
+//     }));
+//
+//     res.send(shirts);
+//   });
 // });
 //
-// const S3_BUCKET = process.env.bucket;
-
-// TODO: ENSURE THIS IS SET TO PRODUCTION WHEN RELEVANT
-var stripe = require('stripe')(process.env.SECRET_KEY);
-
-app.post('/api/create-and-pay-for-order', async (req, res) => {
-  console.log('CREATE-ORDER', req.body);
-
-  stripe.orders.create(req.body.order, (error, order) => {
-    if (error) {
-      res.send({ error });
-    } else {
-      stripe.orders.pay(order.id, {
-        source: req.body.source.id,
-      }, (error, order) => {
-        if (error) {
-          res.send({ error });
-        } else {
-          res.send({ order })
-        }
-      });
-    }
-  });
-});
+// const CHARACTER_LIMIT = 500;
+//
+// app.get('/api/single', async (req, res) => {
+//   const title = req.query.title;
+//
+//   const response = await wtf.fetch(title)
+//     .then((doc) => {
+//       const sentences = doc.sentences().map(({ data: { text } }) => text);
+//
+//       let reachedLimit = false;
+//       let text = '';
+//
+//       for (let i = 0; i < sentences.length; i++) {
+//         const proposedText = text + ' ' + sentences[i];
+//
+//         if (proposedText.length <= CHARACTER_LIMIT && !reachedLimit) {
+//           text = proposedText.replace(/  +/g, ' ').trim();
+//         } else {
+//           reachedLimit = true;
+//         }
+//       }
+//
+//       res.send({
+//         name: doc.options.title,
+//         content: doc.data.sections[0].html(),
+//         text,
+//         links: doc.data.sections[0].links().map(({ page }) => page),
+//         pageId: doc.options.pageID,
+//       });
+//     })
+//     .catch((exception) => {
+//       console.log('Critical error, could\'t view the freaking shirt.');
+//     });
+// });
+//
+// // aws.config.update({
+// //   region: 'ap-southeast-2',
+// //   accessKeyId: process.env.AWSAccessKeyId,
+// //   secretAccessKey: process.env.AWSSecretKey,
+// // });
+// //
+// // const S3_BUCKET = process.env.bucket;
+//
+// // TODO: ENSURE THIS IS SET TO PRODUCTION WHEN RELEVANT
+// var stripe = require('stripe')(process.env.SECRET_KEY);
+//
+// app.post('/api/create-and-pay-for-order', async (req, res) => {
+//   console.log('CREATE-ORDER', req.body);
+//
+//   stripe.orders.create(req.body.order, (error, order) => {
+//     if (error) {
+//       res.send({ error });
+//     } else {
+//       stripe.orders.pay(order.id, {
+//         source: req.body.source.id,
+//       }, (error, order) => {
+//         if (error) {
+//           res.send({ error });
+//         } else {
+//           res.send({ order })
+//         }
+//       });
+//     }
+//   });
+// });
 
 // app.post('/api/upload-image', (req, res) => {
 //   console.log('CALLING /api/upload-image');
@@ -187,4 +188,4 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(port, () => console.log(`Listing on port: ${port}`));
+app.listen(port, () => console.log(`Listening on port: ${port}`));
