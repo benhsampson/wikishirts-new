51132215214f02cba5d104daaa807f335d5d490b
Benjamin Sampson <bensampsondev@gmail.com>
2018-10-24 20:14:04 +1100

    Roll keys and reference through environmental variables

--- client/src/constants/stripe-public-key.js
+++ client/src/constants/stripe-public-key.js
@@ -1,2 +1,2 @@
 // NOTE: ENSURE THIS IS SET TO PRODUCTION TOO
-export default process.env.NODE_ENV === 'production' ? 'pk_live_gwsD9NTi9AFeJZKLwNMyEOsz' : 'pk_test_XzGZ592lSVY2gbFjGyMpxP4Z';
+export default process.env.NODE_ENV === 'production' ? process.env.PUBLIC_KEY_LIVE : process.env.PUBLIC_KEY_TEST;
